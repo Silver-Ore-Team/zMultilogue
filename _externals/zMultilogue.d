d6f183042e7b72295a2cf24b0e00dc6932300dc3
muczc1wek <110698397+muczc1wek@users.noreply.github.com>
2024-09-15 19:28:19 +0200

    Add ZS_MULTILOGUE to _externals

--- _externals/zMultilogue.d
+++ _externals/zMultilogue.d
@@ -18,3 +18,21 @@ func void zMul_Finish() {};
 ///
 /// @param slf NPC to wait for
 func void zMul_Wait(var C_NPC slf) {};
+
+
+/*
+*   To Include:
+*/
+func void ZS_MULTILOGUE() {
+};
+
+func int ZS_MULTILOGUE_Loop() {
+    if (InfoManager_hasFinished()) {
+        return LOOP_END;
+    } else {
+        return LOOP_CONTINUE;
+    };
+};
+
+func void ZS_MULTILOGUE_End() {
+};
